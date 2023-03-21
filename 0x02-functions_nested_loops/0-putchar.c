31ca0fe66b149b85bd0c740112e25f1420cfdad1
Stanley Kariuki <stanleykariuki1999@gmail.com>
2023-03-21 10:51:12 -0700

    prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,15 +8,14 @@
 int main(void)
 
 {
-	_putchar(' ');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char sh[8] = "_putchar";
+	int x;
+
+	for (x=0; x < 8; x++)
+	{
+		_putchar(sh[x]);
+	}
+	        _putchar('\n');
 
 	return (0);
 }
