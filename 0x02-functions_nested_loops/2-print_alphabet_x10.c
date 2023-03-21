b947c6c3179fab08b81e9a8a175cac601f17bf0e
Stanley Kariuki <stanleykariuki1999@gmail.com>
2023-03-21 12:10:37 -0700

    print 10 times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times
+ *
+ * Return: void
+ *
+ * Time complexity: 0(n)
+ */
+void print_alphabet_x10(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
